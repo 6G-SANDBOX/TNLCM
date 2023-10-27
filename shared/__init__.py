339905b6651d50ad9cb19fa68e91dc68e2f30307
nanitebased <bgg@uma.es>
2023-10-27 10:43:27 +0200

    Transfer RestClient, ConfigBase classes

--- shared/__init__.py
+++ shared/__init__.py
@@ -3,3 +3,5 @@ from .io import IO
 from .child import Child
 from .cli_executor import Cli
 from .library import Library, Playbook
+from .rest_client import RestClient, Payload
+from .config_base import ConfigBase, Validable, RestApiConf, LoginRestApiConf, EnabledLoginRestApiConf
