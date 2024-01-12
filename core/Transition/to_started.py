7ebed87cd786f9208ad660a5ce96ab045ffec849
Carlos Andreo <c.andreo@uma.es>
2024-01-12 16:40:01 +0100

    Generalizing and improving connection with Jenkins

--- core/Transition/to_started.py
+++ core/Transition/to_started.py
@@ -7,6 +7,7 @@ from requests import post
 from jenkins import Jenkins
 
 import os
+import yaml
 
 class ToStarted(BaseHandler):
     def __init__(self, trialNetwork: TrialNetwork):
@@ -28,38 +29,50 @@ class ToStarted(BaseHandler):
                 print(f"Unable to instantiate '{entity.Name}': Unknown type '{entity.Description.Type}'")
             sleep(1)
 
-            server = Jenkins(os.getenv("JENKINS_SERVER"), username=os.getenv("JENKINS_USER"), password=os.getenv("JENKINS_PASSWORD"))
+            # Connecting to the jenkins server using python-jenkins API
+            jenkins_client = Jenkins(os.getenv("JENKINS_SERVER"), username=os.getenv("JENKINS_USER"), password=os.getenv("JENKINS_PASSWORD"))
             job_name = "02_Trial_Network_Component"
-            route_file = os.path.join("..", "..", "TNLCM", "DEMO", "file_vxlan.yaml")
-            file_path = os.path.abspath(route_file)
-            parameters = {
-                "TN_ID": "ABCDEF",
-                "LIBRARY_COMPONENT_NAME": "tn_vxlan",
-                "LIBRARY_BRANCH": "first_tn_demo",
-                "DEPLOYMENT_SITE": "uma",
-            }
-            job_url = server.build_job_url(name=job_name, parameters=parameters)
-            with open(file_path, "rb") as file:
-                files = {"FILE": (file_path, file)}
-                response = post(job_url, auth=(os.getenv("JENKINS_USER"), os.getenv("JENKINS_TOKEN")), files=files)
-
-            if response.status_code == 201:
-                # job_info = server.get_job_info(name=job_name)
-                last_build_number = server.get_job_info(name=job_name)["nextBuildNumber"]
-                while last_build_number != server.get_job_info(name=job_name)["lastCompletedBuild"]["number"]:
-                    pass
-
-                if server.get_job_info(name=job_name)["lastSuccessfulBuild"]["number"] == last_build_number:
-                    print("Work")
-                else:
-                    print("Error")
+            route_file = None
+            if name == "VXLAN":
+                route_file = os.path.join("..", "..", "TNLCM", "DEMO", "file_vxlan.yaml")
+            elif name == "BASTION":
+                route_file = os.path.join("..", "..", "TNLCM", "DEMO", "file_bastion.yaml")
             else:
                 print("Error")
-            # TODO: 4 retrieve the values generated during the pipeline:
-            # - Either parse the logs (not so easily scalable)
-            # - Use the callback endpoint (if/when available) to retrieve all values as a dictionary
+            if route_file is not None:
+                file_path = os.path.abspath(route_file)
+                with open(file_path, "rb") as file:
+                    content = yaml.safe_load(file)
+                    tn_id = content.get("tn_id")
+                    component_name = content.get("component_name")
+                    parameters = {
+                        "TN_ID": tn_id,
+                        "LIBRARY_COMPONENT_NAME": component_name,
+                        "LIBRARY_BRANCH": "first_tn_demo",
+                        "DEPLOYMENT_SITE": "uma",
+                    }
+                    job_url = jenkins_client.build_job_url(name=job_name, parameters=parameters)
+                    file.seek(0)
+                    files = {"FILE": (file_path, file)}
+                    response = post(job_url, auth=(os.getenv("JENKINS_USER"), os.getenv("JENKINS_TOKEN")), files=files)
+
+                if response.status_code == 201:
+                    # job_info = server.get_job_info(name=job_name)
+                    last_build_number = jenkins_client.get_job_info(name=job_name)["nextBuildNumber"]
+                    while last_build_number != jenkins_client.get_job_info(name=job_name)["lastCompletedBuild"]["number"]:
+                        pass
+
+                    if jenkins_client.get_job_info(name=job_name)["lastSuccessfulBuild"]["number"] == last_build_number:
+                        print("Work")
+                        # TODO: 4 retrieve the values generated during the pipeline:
+                        # - Either parse the logs (not so easily scalable)
+                        # - Use the callback endpoint (if/when available) to retrieve all values as a dictionary
+
+                    else:
+                        print("Error")
+                else:
+                    print("Error")
 
             entity.Status = Entity.Status.Running
 
         self.tn.CompleteTransition()
-
