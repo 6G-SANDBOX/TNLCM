7e79e2c6918bdb2d9adabd20352cdf9a21dbe9a0
Carlos Andreo <c.andreo@uma.es>
2024-02-03 15:54:15 +0100

    New methods and new sorting of components as the descriptor changes

--- shared/data/trial_network_descriptor.py
+++ shared/data/trial_network_descriptor.py
@@ -7,37 +7,23 @@ class EntityDescriptor:
 
     @property
     def Type(self) -> str:
-        return self.raw['Type']
+        return self.raw.get('Type', str)
 
     @property
     def Parameters(self) -> {}:
         return self.raw.get('Parameters', {})
 
     @property
-    def Connections(self) -> {}:
-        return self.raw.get('Connections', {})
+    def Public(self) -> {}:
+        return self.raw.get('Public', {})
 
     @property
-    def Monitors(self) -> [str]:
-        return self.raw.get('Monitor', [])
+    def Depends_on(self) -> {}:
+        return self.raw.get('Depends_on', {})
 
     @property
-    def DependsOn(self) -> [str]:
-        res = set()
-        for _, entity in self.Connections.items():
-            res.add(entity.split('.')[0])
-        for entity in self.Monitors:
-            res.add(entity.split('.')[0])
-        return list(res)
-
-    @property
-    def IsRequiredFor(self) -> [str]:
-        """Convenience property, this information is handled by the TND"""
-        res = self.parent.GetEntityIsRequiredFor(self.Name)
-        if res is None:
-            raise RuntimeError(f"Inconsistent TND: Entity {self.Name} exists,"
-                               f" but TND does not know if it's required for others")
-        return res
+    def Metadata(self):
+        return self.raw.get('Metadata', {})
 
     @property
     def Serialized(self):
@@ -45,12 +31,8 @@ class EntityDescriptor:
             'name': self.Name,
             'type': self.Type,
             'parameters': self.Parameters,
-            'connections': self.Connections,
-            'monitors': self.Monitors,
-            'depends_on': self.DependsOn,
-            'is_required_for': self.IsRequiredFor
+            'depends_on': self.Depends_on,
         }
-
 
 class TrialNetworkDescriptor:
     """
@@ -61,10 +43,9 @@ class TrialNetworkDescriptor:
     def __init__(self, rawDescriptor: {}):
         self.Raw = rawDescriptor
         self.Entities: {str, EntityDescriptor} = {}
-        self.requiredFor: {str, [str]} = {}
         self.Valid = False
         self.ValidationReport: [str] = []
-        self.DeploymentOrder: [str] = []
+        self.DeploymentOrder: [] = []
 
         hasErrors = False
 
@@ -72,16 +53,6 @@ class TrialNetworkDescriptor:
         for name, definition in entities.items():
             instance = EntityDescriptor(name, definition, self)
             self.Entities[name] = instance
-            self.requiredFor[name] = []  # First create all entities and fill the dict
-
-        # Then traverse entities and fill the lists
-        for name, entity in self.Entities.items():
-            for depends in entity.DependsOn:
-                try:
-                    self.requiredFor[depends].append(name)
-                except KeyError:
-                    hasErrors = True
-                    self.ValidationReport.append(f"Entity '{entity.Name}' depends on '{depends}', which does not exist.")
 
         order, maybeError = self.calculateDeploymentOrder()
 
@@ -96,37 +67,28 @@ class TrialNetworkDescriptor:
         if not hasErrors:
             self.Valid = True
 
-    def calculateDeploymentOrder(self) -> ([], str | None):
-        def _checkDependencies(item: str) -> (str, None):
-            for dependency in self.Entities[item].DependsOn:
-                if dependency not in order:
-                    _ = sortedbydependent.remove(dependency)
-                    order.insert(0, dependency)
-                    return _checkDependencies(dependency)
-                else:
-                    itemPosition = order.index(item)
-                    dependencyPosition = order.index(dependency)
-                    if itemPosition > dependencyPosition:
-                        pass  # We have a dependency, but will be deployed before this element
-                    else:
-                        return f"Detected a dependency cycle that includes elements '{item}' and '{dependency}'"
-            return None  # No issues detected
+    def calculateDeploymentOrder(self) -> ([], None):
+        res = []
+        visited = set()
 
-        # Build dependency graph
-        sortedbydependent = sorted(self.requiredFor.keys(), key=lambda e: len(self.requiredFor[e]), reverse=True)
-        order = []
+        def visit(entity_name):
+            if entity_name in visited:
+                return
+            visited.add(entity_name)
 
-        while len(sortedbydependent) > 0:
-            item = sortedbydependent.pop(0)
-            order.append(item)
-            maybeError = _checkDependencies(item)
-            if maybeError is not None:
-                return [], maybeError
+            entity = self.Entities[entity_name]
+            dependencies = entity.Depends_on.keys()
 
-        return order, None
+            for dep_name in dependencies:
+                visit(dep_name)
 
-    def GetEntityIsRequiredFor(self, name: str) -> ([str], None):
-        return self.requiredFor.get(name, None)
+            res.append(entity)
+
+        # Visit each entity to perform topological sorting
+        for entity_name in self.Entities.keys():
+            visit(entity_name)
+
+        return res, None
 
     def __str__(self):
         return f'[TND] -> {[f"{e.Name}:{e.Type}"  for e in self.Entities.values()]}'
