76c4a1f1fdf5fd7bc3fbdd4facc4d4269c9afef4
Carlos Andreo <c.andreo@uma.es>
2024-02-29 22:25:34 +0100

    Rename function deploy component and add get job name function

--- core/Transition/jenkins_handler.py
+++ core/Transition/jenkins_handler.py
@@ -10,13 +10,17 @@ class JenkinsHandler:
         self.jenkins_user = os.getenv("JENKINS_USER")
         self.jenkins_password = os.getenv("JENKINS_PASSWORD")
         self.jenkins_token = os.getenv("JENKINS_TOKEN")
+        self.jenkins_job_name = os.getenv("JENKINS_JOB_NAME")
         self.jenkins_client = Jenkins(self.jenkins_server, username=self.jenkins_user, password=self.jenkins_password)
 
     def get_jenkins_client(self):
         return self.jenkins_client
 
-    def deploy_components(self, job_url, files):
-        return post(job_url, auth=(self.jenkins_user, self.jenkins_token), files=files)
+    def get_job_name(self):
+        return self.jenkins_job_name
+
+    def deploy_component(self, job_url, file):
+        return post(job_url, auth=(self.jenkins_user, self.jenkins_token), files=file)
 
     def update_marketplace(self):
         # TODO: pipeline to update the TNLCM version in marketplace
