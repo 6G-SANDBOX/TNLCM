c5b1f5202032f3b8c5fc620b785a5e9f2a0472e0
Carlos Andreo <c.andreo@uma.es>
2024-03-12 13:14:54 +0100

    First commit

--- core/Composer/composer.py
+++
@@ -1,9 +0,0 @@
-from shared.data import TrialNetworkDescriptor, EntityDescriptor
-
-
-class Composer:
-    @classmethod
-    def Compose(cls, rawDescriptor: {}) -> TrialNetworkDescriptor:
-        res = TrialNetworkDescriptor(rawDescriptor)
-
-        return res
