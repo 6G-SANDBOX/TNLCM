aafe2ab1194ed941f2e01aab53a6129af741faff
Carlos Andreo <c.andreo@uma.es>
2024-03-14 22:21:49 +0100

    Add main to start TNLCM

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
-from flask import Flask, current_app
+import os
+
+from flask import Flask
 from flask_restx import Api
 from flask_cors import CORS
 from waitress import serve
 
 from config import Config
+from src.routes import trial_network_namespace, callback_namespace
 
 app = Flask(__name__)
 CORS(app)
@@ -17,12 +20,14 @@ api = Api(
     # doc=False
 )
 
+api.add_namespace(trial_network_namespace, path="/api/trial_network")
+api.add_namespace(callback_namespace, path="/api/callback")
+
 if __name__ == "__main__":
-    with app.app_context():
-        flask_env = current_app.config["FLASK_ENV"]
-        if flask_env == "DEVELOPMENT":
-            app.run(host="0.0.0.0", port=5000, debug=True)
-        elif flask_env == "PRODUCTION":
-            serve(app, host="0.0.0.0", port=5000)
-        else:
-            app.run(host="0.0.0.0", port=5000)
+    flask_env = os.getenv("FLASK_ENV")
+    if flask_env == "DEVELOPMENT":
+        app.run(host="0.0.0.0", port=5000, debug=True)
+    elif flask_env == "PRODUCTION":
+        serve(app, host="0.0.0.0", port=5000)
+    else:
+        app.run(host="0.0.0.0", port=5000)
