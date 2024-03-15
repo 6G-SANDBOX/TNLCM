d5dab14b6233c0b28900192e7e2ee40016fe045a
Carlos Andreo <c.andreo@uma.es>
2024-03-15 23:57:23 +0100

    Add repository to manage the TNLCM relationship with the 6GLibrary

---
+++ src/repository/repository_handler.py
@@ -0,0 +1,44 @@
+import os
+import stat
+
+from shutil import rmtree
+from git import Repo, exc
+
+repository_directory = os.path.join(os.getcwd(), "src", "repository")
+
+class RepositoryHandler:
+
+    @staticmethod
+    def onerror(func, path, exc_info):
+        """
+        Error handler for ``shutil.rmtree``.
+
+        If the error is due to an access error (read only file)
+        it attempts to add write permission and then retries.
+
+        If the error is for another reason it re-raises the error.
+        
+        Usage : ``shutil.rmtree(path, onerror=onerror)``
+        """
+        if not os.access(path, os.W_OK):
+            os.chmod(path, stat.S_IWUSR)
+            func(path)
+        else:
+            raise
+
+    def __init__(self, git_url):
+        self.git_url = git_url
+        if not os.path.exists(repository_directory):
+            os.makedirs(repository_directory)
+
+    def clone_repository(self, repository_name, branch):
+        try:
+            local_directory = os.path.join(repository_directory, repository_name)
+            if os.path.exists(local_directory):
+                rmtree(local_directory, onerror=self.onerror)
+            Repo.clone_from(self.git_url, local_directory, branch=branch)
+        except exc.GitCommandError as e:
+            print(e)
+
+    def extract_components(self):
+        pass
