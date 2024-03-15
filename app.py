05ed0d48eb77e6623249664ad263d74500263173
Carlos Andreo <c.andreo@uma.es>
2024-03-15 23:58:44 +0100

    Add clone 6GLibrary repository when server start

--- app.py
+++ app.py
@@ -8,10 +8,13 @@ from waitress import serve
 from config import Config
 from src.routes import trial_network_namespace, callback_namespace
 from logs.log_handler import LogHandler
+from src.repository.repository_handler import RepositoryHandler
+from dotenv import load_dotenv
 
 app = Flask(__name__)
 CORS(app)
 app.config.from_object(Config)
+load_dotenv()
 
 api = Api(
     app,
@@ -26,6 +29,8 @@ api.add_namespace(callback_namespace, path="/api/callback")
 
 if __name__ == "__main__":
     LogHandler()
+    sixg_repository = RepositoryHandler(os.getenv("6GLIBRARY_REPOSITORY_HTTPS"))
+    sixg_repository.clone_repository("6glibrary", "main")
     flask_env = os.getenv("FLASK_ENV")
     if flask_env == "DEVELOPMENT":
         app.run(host="0.0.0.0", port=5000, debug=True)
