08504e52263f17e9e832ed804c3b3a34e3299a84
Carlos Andreo <c.andreo@uma.es>
2024-03-16 22:25:47 +0100

    Add control exceptions

--- src/routes/trial_network.py
+++ src/routes/trial_network.py
@@ -2,9 +2,11 @@ from flask_restx import Namespace, Resource, reqparse, abort
 from werkzeug.datastructures import FileStorage
 from werkzeug.utils import secure_filename
 from yaml import safe_load, YAMLError
+from pymongo.errors import ConnectionFailure
+from jenkins import JenkinsException
 
 from src.trial_network.trial_network_queries import get_trial_networks, create_trial_network, get_descriptor_trial_network
-from src.callback.jenkins_functions import deploy_trial_network
+from src.callback.jenkins_handler import JenkinsHandler
 
 trial_network_namespace = Namespace(
     name="trial_network",
@@ -22,20 +24,30 @@ class CreateTrialNetwork(Resource):
         """
         Create and add a Trial Network
         """
-        descriptor_file = self.parser.parse_args()["descriptor"]
-        filename = secure_filename(descriptor_file.filename)
-        if '.' in filename and filename.split('.')[-1].lower() in ['yml', 'yaml']:
-            try:
+        try:
+            descriptor_file = self.parser.parse_args()["descriptor"]
+            filename = secure_filename(descriptor_file.filename)
+            if '.' in filename and filename.split('.')[-1].lower() in ['yml', 'yaml']:
                 descriptor = safe_load(descriptor_file.stream)
-            except YAMLError as e:
-                return abort(422, f"Malformed or insecure descriptor received: '{e}'.")
-            tn_id = create_trial_network(descriptor)
-            return {'id': tn_id}, 200
-        else:
-            return abort(400, "Invalid descriptor format, only 'yml' or 'yaml' files will be further processed.")
+                tn_id = create_trial_network(descriptor)
+                return {"tn_id": tn_id}, 200
+            else:
+                return abort(400, "Invalid descriptor format, only 'yml' or 'yaml' files will be further processed.")
+        except ValueError as e:
+            return abort(404, e)
+        except ConnectionFailure as e:
+            return abort(408, e)
+        except YAMLError as e:
+            return abort(422, f"Malformed or insecure descriptor received: '{e}'.")
+        except Exception as e:
+            return abort(422, e)
 
 @trial_network_namespace.route("/<string:tn_id>")
 class DeployTrialNetwork(Resource):
+
+    def __init__(self, api):
+        self.api = api
+        self.jenkins_handler = JenkinsHandler()
 
     def get(self, tn_id):
         """
@@ -46,6 +58,8 @@ class DeployTrialNetwork(Resource):
             return sorted_descriptor, 200
         except ValueError as e:
             return abort(404, e)
+        except ConnectionFailure as e:
+            return abort(408, e)
         except Exception as e:
             return abort(422, e)
 
@@ -54,10 +68,16 @@ class DeployTrialNetwork(Resource):
         Trial Network component deployment begins
         """
         try:
-            deploy_trial_network(tn_id)
+            self.jenkins_handler.deploy_trial_network(tn_id)
             return {"message": "Trial Network start deployment with jenkins"}, 200
         except ValueError as e:
             return abort(404, e)
+        except JenkinsException as e:
+            return abort(404, e)
+        except TimeoutError as e:
+            return abort(408, e)
+        except ConnectionFailure as e:
+            return abort(408, e)
         except Exception as e:
             return abort(422, e)
 
@@ -73,5 +93,7 @@ class TrialNetworks(Resource):
             return {"tn_ids": trial_networks}, 200
         except ValueError as e:
             return abort(404, e)
+        except ConnectionFailure as e:
+            return abort(408, e)
         except Exception as e:
             return abort(422, e)
--- src/trial_network/trial_network_queries.py
+++ src/trial_network/trial_network_queries.py
@@ -4,49 +4,64 @@ from json import dumps, loads
 from src.database.mongo_handler import MongoHandler
 from src.trial_network.trial_network_descriptor import sort_descriptor
 
-STATUS_TRIAL_NETWORK = ["pending", "deploying", "finished"]
+STATUS_TRIAL_NETWORK = ["pending", "deploying", "finished", "failed"]
 
 def create_mongo_client():
-    mongo_client = MongoHandler()
-    return mongo_client
+    try:
+        mongo_client = MongoHandler()
+        return mongo_client
+    except:
+        raise
 
 def get_trial_networks():
-    mongo_client = create_mongo_client()
-    projection = {"_id": 0, "tn_id": 1}
-    trial_networks = mongo_client.find_data(collection_name="trial_network", projection=projection)
-    mongo_client.disconnect()
-    if trial_networks:
+    try:
+        mongo_client = create_mongo_client()
+        projection = {"_id": 0, "tn_id": 1}
+        trial_networks = mongo_client.find_data(collection_name="trial_network", projection=projection)
+        mongo_client.disconnect()
         return [tn["tn_id"] for tn in trial_networks]
+    except:
+        raise
 
 def create_trial_network(descriptor):
-    mongo_client = create_mongo_client()
-    tn_id = str(uuid4())
-    tn_status = STATUS_TRIAL_NETWORK[0]
-    tn_raw_descriptor_json = dumps(descriptor)
-    tn_sorted_descriptor_json = dumps(sort_descriptor(descriptor))
-    trial_network_doc = {
-        "tn_id": tn_id,
-        "tn_status": tn_status,
-        "tn_raw_descriptor": tn_raw_descriptor_json,
-        "tn_sorted_descriptor": tn_sorted_descriptor_json
-    }
-    mongo_client.insert_data("trial_network", trial_network_doc)
-    mongo_client.disconnect()
-    return tn_id
+    try:
+        mongo_client = create_mongo_client()
+        tn_id = str(uuid4())
+        tn_status = STATUS_TRIAL_NETWORK[0]
+        tn_raw_descriptor_json = dumps(descriptor)
+        tn_sorted_descriptor_json = dumps(sort_descriptor(descriptor))
+        trial_network_doc = {
+            "tn_id": tn_id,
+            "tn_status": tn_status,
+            "tn_raw_descriptor": tn_raw_descriptor_json,
+            "tn_sorted_descriptor": tn_sorted_descriptor_json
+        }
+        mongo_client.insert_data("trial_network", trial_network_doc)
+        mongo_client.disconnect()
+        return tn_id
+    except:
+        raise
 
 def get_descriptor_trial_network(tn_id):
-    mongo_client = create_mongo_client()
-    query = {"tn_id": tn_id}
-    projection = {"_id": 0, "tn_sorted_descriptor": 1}
-    trial_network_descriptor = mongo_client.find_data(collection_name="trial_network", query=query, projection=projection)
-    mongo_client.disconnect()
-    if trial_network_descriptor:
+    try:
+        mongo_client = create_mongo_client()
+        query = {"tn_id": tn_id}
+        projection = {"_id": 0, "tn_sorted_descriptor": 1}
+        trial_network_descriptor = mongo_client.find_data(collection_name="trial_network", query=query, projection=projection)
+        mongo_client.disconnect()
         return loads(trial_network_descriptor[0]["tn_sorted_descriptor"])
+    except:
+        raise
 
 def update_status_trial_network(tn_id, new_status):
-    # check if new status is in STATUS_TRIAL_NETWORK
-    mongo_client = create_mongo_client()
-    query = {"tn_id": tn_id}
-    update = {"$set": {"tn_status": new_status}}
-    mongo_client.update_data("trial_network", query, update)
-    mongo_client.disconnect()
+    try:
+        if new_status in STATUS_TRIAL_NETWORK:
+            mongo_client = create_mongo_client()
+            query = {"tn_id": tn_id}
+            update = {"$set": {"tn_status": new_status}}
+            mongo_client.update_data("trial_network", query, update)
+            mongo_client.disconnect()
+        else:
+            raise ValueError(f"The status cannot be updated. The possible states are: {STATUS_TRIAL_NETWORK}")
+    except:
+        raise
