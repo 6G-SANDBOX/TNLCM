f1730028e989f5aae708196fe5dd41dec5a9d12e
Carlos Andreo <c.andreo@uma.es>
2024-03-16 23:28:54 +0100

    Fix when library is cloned

--- src/repository/repository_handler.py
+++ src/repository/repository_handler.py
@@ -25,18 +25,18 @@ class RepositoryHandler:
         self.git_commit_id = git_commit_id
         self.local_directory = os.path.join(repository_directory, repository_name)
         self.repo = None
-        if not os.path.exists(self.local_directory):
-            os.makedirs(self.local_directory)
-        else:
+        if os.path.exists(self.local_directory):
             self.repo = Repo(self.local_directory)
-            if self.last_git_clone() == "commit" and self.git_branch:
+            last_clone_type = self.last_git_clone()
+
+            if (last_clone_type == "commit" and self.git_branch) or \
+                    (last_clone_type == "commit" and self.git_commit_id and not self.is_current_commit_id()) or \
+                    (last_clone_type == "branch" and self.git_commit_id) or \
+                    (last_clone_type == "branch" and self.git_branch and not self.is_current_branch()):
                 rmtree(self.local_directory, onerror=remove_readonly)
-            elif self.last_git_clone() == "commit" and self.git_commit_id and not self.is_current_commit_id():
-                rmtree(self.local_directory, onerror=remove_readonly)
-            elif self.last_git_clone() == "branch" and self.git_commit_id:
-                rmtree(self.local_directory, onerror=remove_readonly)
-            elif self.last_git_clone() == "branch" and self.git_branch and not self.is_current_branch():
-                rmtree(self.local_directory, onerror=remove_readonly)
+                self.repo = None
+        else:
+            os.makedirs(self.local_directory)
 
     def is_current_branch(self):
         return self.repo.active_branch.name == self.git_branch
@@ -52,7 +52,10 @@ class RepositoryHandler:
     
     def git_clone_repository(self):
         try:
-            self.repo = Repo.clone_from(self.git_url, self.local_directory)
+            if self.repo is None:
+                self.repo = Repo.clone_from(self.git_url, self.local_directory)
+            else:
+                raise GitError("6G-Library is already cloned")
         except InvalidGitRepositoryError as e:
             raise InvalidGitRepositoryError(e)
         except GitCommandError as e:
