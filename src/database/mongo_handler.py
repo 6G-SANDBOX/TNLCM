b706e2a02cb1c081781783d64715edb26e6cc621
Carlos Andreo <c.andreo@uma.es>
2024-03-16 22:26:06 +0100

    Fix some errors

--- src/database/mongo_handler.py
+++ src/database/mongo_handler.py
@@ -1,40 +1,65 @@
 import os
 
 from pymongo import MongoClient
+from pymongo.errors import ConnectionFailure
+
+COLLECTIONS = ["trial_network"]
 
 class MongoHandler:
 
     def __init__(self):
-        self.database = os.getenv("MONGO_DATABASE")
-        self.uri = os.getenv("MONGO_URI")
-        self.client = MongoClient(self.uri)
-        self.db = self.client[self.database]
+        try:
+            self.database = os.getenv("MONGO_DATABASE")
+            self.uri = os.getenv("MONGO_URI")
+            self.client = MongoClient(self.uri)
+            self.db = self.client[self.database]
+        except Exception as e:
+            raise Exception(e)
 
     def disconnect(self):
         try:
-            if self.client:
-                self.client.close()
+            self.client.close()
         except Exception as e:
-            raise Exception("Failed to disconnect to MongoDB")
+            raise Exception(e)
     
     def insert_data(self, collection_name, doc):
-        try:
-            collection = self.db[collection_name]
-            collection.insert_one(doc)
-        except Exception as e:
-            raise Exception(e)
+        if collection_name in COLLECTIONS:
+            try:
+                collection = self.db[collection_name]
+                collection.insert_one(doc)
+            except ConnectionFailure:
+                raise ConnectionFailure("Unable to connect to database")
+            except Exception as e:
+                raise Exception(e)
+        else:
+            raise ValueError("Collection not found") 
     
     def find_data(self, collection_name, query=None, projection=None):
-        try:
-            collection = self.db[collection_name]
-            result = collection.find(query, projection)
-            return list(result)
-        except Exception as e:
-            raise Exception(e)
+        if collection_name in COLLECTIONS:
+            result = None
+            try:
+                collection = self.db[collection_name]
+                result = list(collection.find(query, projection))
+            except ConnectionFailure:
+                raise ConnectionFailure("Unable to connect to database")
+            except Exception as e:
+                raise Exception(e)
+            if result:
+                return result
+            else:
+                raise ValueError("No results found in the database")
+        else:
+            raise ValueError("Collection not found")
+        
     
     def update_data(self, collection_name, query, update):
-        try:
-            collection = self.db[collection_name]
-            collection.update_one(query, update)
-        except Exception as e:
-            raise Exception(e)
+        if collection_name in COLLECTIONS:
+            try:
+                collection = self.db[collection_name]
+                collection.update_one(query, update)
+            except ConnectionFailure:
+                raise ConnectionFailure("Unable to connect to database")
+            except Exception as e:
+                raise Exception(e)
+        else:
+            raise ValueError("Collection not found")
