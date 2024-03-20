0a414f8b5712c7040e2929f772e60774cc138de6
Carlos Andreo <c.andreo@uma.es>
2024-03-20 10:31:01 +0100

    Add templates but not implement yes

--- src/database/tnlcm-structure.js
+++ src/database/tnlcm-structure.js
@@ -1,6 +1,8 @@
 var dbName = 'tnlcm-database';
 var trialNetworkCollection = 'trial_network';
+var trialNetworksTemplates = 'trial_networks_templates';
 
 var db = db.getSiblingDB(dbName);
 
 db.createCollection(trialNetworkCollection);
+db.createCollection(trialNetworksTemplates);
