38a58be107901adf27477a46c734ce2a079c07d7
Carlos Andreo <c.andreo@uma.es>
2024-03-27 09:59:36 +0100

    Update descriptor and update temp file

--- src/temp/temp_file_handler.py
+++ src/temp/temp_file_handler.py
@@ -11,15 +11,13 @@ class TempFileHandler:
         """Constructor"""
         os.makedirs(TEMP_FILES_PATH, exist_ok=True)
     
-    def create_component_temp_file(self, component_name, component_public, tn_vxlan_id=None):
+    def create_component_temp_file(self, component_name, entity_public, tn_vxlan_id=None):
         """Create temporary files for each component that is deployed in the pipeline and returns the path to the file"""
         with NamedTemporaryFile(delete=False, dir=TEMP_FILES_PATH, suffix=".yaml", mode='w') as component_temp_file:
-            if component_public is None:
-                component_public = {}
-            component_public["tnlcm_callback"] = os.getenv("CALLBACK_URL")
+            entity_public["tnlcm_callback"] = os.getenv("CALLBACK_URL")
             if tn_vxlan_id is not None:
-                component_public["one_component_networks"] = [0, int(tn_vxlan_id)]
+                entity_public["one_component_networks"] = [0, int(tn_vxlan_id)]
                 if component_name == "tn_bastion":
-                    component_public["one_bastion_wireguard_allowed_networks"] = "192.168.199.0/24"
-            dump(component_public, component_temp_file, default_flow_style=False)
+                    entity_public["one_bastion_wireguard_allowed_networks"] = "192.168.199.0/24"
+            dump(entity_public, component_temp_file, default_flow_style=False)
         return component_temp_file.name
