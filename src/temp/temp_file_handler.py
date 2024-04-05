b2b784e1487a5c24d3aabdeb136b79ed8baff97f
Carlos Andreo <c.andreo@uma.es>
2024-04-05 19:15:12 +0200

    Update for code division

--- src/temp/temp_file_handler.py
+++ src/temp/temp_file_handler.py
@@ -28,15 +28,21 @@ class TempFileHandler:
                                 tn_vxlan_ids.append(int(tn_vxlan_id))
         return tn_vxlan_ids
 
-    def create_entity_temp_file(self, entity_data, descriptor_trial_network, report_directory, tn_id):
-        """Create temporary files for each entity that is deployed in the pipeline and returns the path to the file"""
+    def add_private_entity_parameters(self, entity_data, descriptor_trial_network, report_directory, tn_id):
+        """Add private parameters to the entity file"""
         entity_public = entity_data["public"]
         entity_type = entity_data["type"]
+        entity_public["tnlcm_callback"] = os.getenv("CALLBACK_URL")
+        if entity_type == "tn_bastion":
+            entity_public["one_component_networks"] = [0] + self.extract_tn_vxlan_ids(entity_data, descriptor_trial_network, report_directory, tn_id)
+            entity_public["one_bastion_wireguard_allowed_networks"] = "192.168.199.0/24"
+        elif entity_type == "vm_kvm_very_small" or entity_type == "vm_kvm_small" or entity_type == "vm_kvm_medium" or entity_type == "vm_kvm_large" or entity_type == "vm_kvm_extra_large":
+            entity_public["one_component_networks"] = [0] + self.extract_tn_vxlan_ids(entity_data, descriptor_trial_network, report_directory, tn_id)
+        return entity_public
+
+    def create_entity_temp_file(self, entity_data, descriptor_trial_network, report_directory, tn_id):
+        """Create temporary files for each entity that is deployed in the pipeline and returns the path to the file"""
         with NamedTemporaryFile(delete=False, dir=TEMP_FILES_PATH, suffix=".yaml", mode='w') as entity_temp_file:
-            entity_public["tnlcm_callback"] = os.getenv("CALLBACK_URL")
-            if entity_type != "tn_vxlan":
-                entity_public["one_component_networks"] = [0] + self.extract_tn_vxlan_ids(entity_data, descriptor_trial_network, report_directory, tn_id)
-                if entity_type == "tn_bastion":
-                    entity_public["one_bastion_wireguard_allowed_networks"] = "192.168.199.0/24"
+            entity_public = self.add_private_entity_parameters(entity_data, descriptor_trial_network, report_directory, tn_id)
             dump(entity_public, entity_temp_file, default_flow_style=False)
         return entity_temp_file.name
