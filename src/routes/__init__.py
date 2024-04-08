8fbbeee9dfa184e502f98b2c27026e5f20ac8de2
Carlos Andreo <c.andreo@uma.es>
2024-04-08 15:54:28 +0200

    Add verification namespace

--- src/routes/__init__.py
+++ src/routes/__init__.py
@@ -2,3 +2,4 @@ from .callback import callback_namespace
 from .trial_network import trial_network_namespace
 from .sixglibrary import sixglibrary_namespace
 from .users import users_namespace
+from .verification import verification_namespace
