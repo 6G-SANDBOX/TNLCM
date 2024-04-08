3e26206ae7f2b952334a427b311e8ed770ad7b6f
Carlos Andreo <c.andreo@uma.es>
2024-04-08 18:10:29 +0200

    Fix update creation date verification token

--- src/verification/verification_handler.py
+++ src/verification/verification_handler.py
@@ -5,12 +5,14 @@ from src.database.mongo_handler import MongoHandler
 class VerificationHandler:
 
     def __init__(self, new_account_email, verification_token):
+        """Constructor"""
         self.new_account_email = new_account_email
         self.verification_token = verification_token
         self.creation_date = datetime.now(timezone.utc).strftime("%Y-%m-%d %H:%M:%S")
         self.mongo_client = MongoHandler()
     
     def add_verification_token(self):
+        """Add verification token to database"""
         verification_token_doc = {
             "new_account_email": self.new_account_email,
             "verification_token": self.verification_token,
@@ -19,11 +21,13 @@ class VerificationHandler:
         self.mongo_client.insert_data("verification_tokens", verification_token_doc)
     
     def update_verification_token(self):
+        """Update verification token"""
         query = {"new_account_email": self.new_account_email}
-        projection = {"$set": {"verification_token": self.verification_token}}
+        projection = {"$set": {"verification_token": self.verification_token, "creation_date": self.creation_date}}
         self.mongo_client.update_data(collection_name="verification_tokens", query=query, projection=projection)
 
     def get_verification_token(self):
+        """Return verification token associated an email of user"""
         query = {"new_account_email": self.new_account_email, "verification_token": self.verification_token}
         projection = {"_id": 0, "verification_token": 1}
         verification_token = self.mongo_client.find_data(collection_name="verification_tokens", query=query, projection=projection)
