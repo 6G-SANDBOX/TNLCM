2aca6bef4b1863b48981ac8779aef1a58ef9cb46
Carlos Andreo <c.andreo@uma.es>
2024-04-08 16:50:55 +0200

    Delete print

--- src/verification/verification_handler.py
+++ src/verification/verification_handler.py
@@ -22,5 +22,4 @@ class VerificationHandler:
         query = {"new_account_email": self.new_account_email, "verification_token": self.verification_token}
         projection = {"_id": 0, "verification_token": 1}
         verification_token = self.mongo_client.find_data(collection_name="verification_tokens", query=query, projection=projection)
-        print(verification_token)
         return verification_token
