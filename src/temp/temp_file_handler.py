32202f8284f0f0806098463acc1acbfeddffdcd7
Carlos Andreo <c.andreo@uma.es>
2024-04-09 19:05:45 +0200

    Add find vxlan id

--- src/temp/temp_file_handler.py
+++ src/temp/temp_file_handler.py
@@ -27,6 +27,18 @@ class TempFileHandler:
                                 tn_vxlan_id = json_data["tn_vxlan_id"]
                                 tn_vxlan_ids.append(int(tn_vxlan_id))
         return tn_vxlan_ids
+    
+    def find_vxlan_id(self, vnet_name, tn_descriptor, report_directory, tn_id):
+        vnet_id = None
+        for descriptor_entities_name, descriptor_entities_data in tn_descriptor.items():
+            if descriptor_entities_name == vnet_name and descriptor_entities_data["type"] == "tn_vxlan" or descriptor_entities_data["type"] == "vxlan":
+                descriptor_entity_report_file = os.path.join(report_directory, descriptor_entities_name + "_" + tn_id + ".json")
+                if os.path.isfile(descriptor_entity_report_file):
+                    with open(descriptor_entity_report_file, "r") as file:
+                        json_data = load(file)
+                        tn_vxlan_id = json_data["tn_vxlan_id"]
+                        vnet_id = int(tn_vxlan_id)
+        return vnet_id
 
     def add_private_entity_parameters(self, entity_data, tn_descriptor, report_directory, tn_id):
         """Add private parameters to the entity file"""
@@ -38,6 +50,9 @@ class TempFileHandler:
             entity_public["one_bastion_wireguard_allowed_networks"] = "192.168.199.0/24"
         elif entity_type == "vm_kvm_very_small" or entity_type == "vm_kvm_small" or entity_type == "vm_kvm_medium" or entity_type == "vm_kvm_large" or entity_type == "vm_kvm_extra_large":
             entity_public["one_component_networks"] = [0] + self.extract_tn_vxlan_ids(entity_data, tn_descriptor, report_directory, tn_id)
+        elif entity_type == "k8s_medium":
+            entity_public["external_vnet_id"] = self.find_vxlan_id(entity_public["external_vnet_id"], tn_descriptor, report_directory, tn_id)
+            entity_public["internal_vnet_id"] = self.find_vxlan_id(entity_public["internal_vnet_id"], tn_descriptor, report_directory, tn_id)
         return entity_public
 
     def create_entity_temp_file(self, entity_data, tn_descriptor, report_directory, tn_id):
