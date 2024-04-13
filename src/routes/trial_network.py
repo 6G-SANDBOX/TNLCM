0f9530752ea9d24617755e1b5d2944cd45e14cbf
CarlosAndreo <c.andreo@uma.es>
2024-04-13 22:54:07 +0200

    Fix route templates

--- src/routes/trial_network.py
+++ src/routes/trial_network.py
@@ -249,7 +249,7 @@ class TrialNetworksStatus(Resource):
             if trial_network_handler is not None:
                 trial_network_handler.mongo_client.disconnect()
 
-@trial_network_namespace.route("s/templates/")
+@trial_network_namespace.route("s/templates")
 class TrialNetworksTemplates(Resource):
 
     @trial_network_namespace.doc(security="Bearer Auth")
