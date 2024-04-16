dcdfabd526b08e2e961d56121432ac9d2fa9169e
Carlos Andreo <c.andreo@uma.es>
2024-04-16 15:01:17 +0200

    Minor changes

--- src/database/tnlcm-structure.js
+++ src/database/tnlcm-structure.js
@@ -2,11 +2,11 @@ var dbName = 'tnlcm-database';
 var trialNetworks = 'trial_networks';
 var trialNetworksTemplates = 'trial_networks_templates';
 var users = 'users';
-var verification_tokens = 'verification_tokens';
+var verificationTokens = 'verification_tokens';
 
 var db = db.getSiblingDB(dbName);
 
 db.createCollection(trialNetworks);
 db.createCollection(trialNetworksTemplates);
 db.createCollection(users)
-db.createCollection(verification_tokens)
+db.createCollection(verificationTokens)
