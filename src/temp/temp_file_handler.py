6b95982f72dc075ad7be7e13d37f89a70669d749
CarlosAndreo <c.andreo@uma.es>
2024-04-30 20:03:02 +0200

    Remove tnlcm callback in private parameters

--- src/temp/temp_file_handler.py
+++ src/temp/temp_file_handler.py
@@ -25,6 +25,7 @@ class TempFileHandler:
         return vnets_id
 
     def _get_vxlan_ids(self, vxlan_name, tn_descriptor, report_directory, tn_id):
+        """Return list with vxlan ids"""
         vxlan_ids = []
         for descriptor_entities_name, descriptor_entities_data in tn_descriptor.items():
             if descriptor_entities_name == vxlan_name and (descriptor_entities_data["type"] == "tn_vxlan" or descriptor_entities_data["type"] == "vxlan"):
@@ -39,25 +40,24 @@ class TempFileHandler:
     def _add_private_entity_parameters(self, entity_name, entity_data, tn_descriptor, report_directory, tn_id):
         """Add private parameters to the entity file"""
         log_handler.info(f"Adding private part to entity '{entity_name}'")
-        entity_public = entity_data["public"]
+        entity_input = entity_data["input"]
         entity_type = entity_data["type"]
-        entity_public["tnlcm_callback"] = os.getenv("CALLBACK_URL")
         if entity_type == "tn_bastion":
-            entity_public["one_component_networks"] = [280] + self.find_vxlan_ids(entity_public["one_component_networks"], tn_descriptor, report_directory, tn_id)
-            entity_public["one_bastion_wireguard_allowed_networks"] = "192.168.199.0/24"
+            entity_input["one_component_networks"] = [280] + self.find_vxlan_ids(entity_input["one_component_networks"], tn_descriptor, report_directory, tn_id)
+            entity_input["one_bastion_wireguard_allowed_networks"] = "192.168.199.0/24"
         elif entity_type == "vm_kvm_very_small" or entity_type == "vm_kvm_small" or entity_type == "vm_kvm_medium" or entity_type == "vm_kvm_large" or entity_type == "vm_kvm_extra_large":
-            entity_public["one_component_networks"] = [0] + self.find_vxlan_ids(entity_public["one_component_networks"], tn_descriptor, report_directory, tn_id)
+            entity_input["one_component_networks"] = [0] + self.find_vxlan_ids(entity_input["one_component_networks"], tn_descriptor, report_directory, tn_id)
         elif entity_type == "k8s_medium":
-            entity_public["external_vnet_id"] = self.find_vxlan_ids(entity_public["external_vnet_id"], tn_descriptor, report_directory, tn_id)
-            entity_public["internal_vnet_id"] = self.find_vxlan_ids(entity_public["internal_vnet_id"], tn_descriptor, report_directory, tn_id)
+            entity_input["external_vnet_id"] = self.find_vxlan_ids(entity_input["external_vnet_id"], tn_descriptor, report_directory, tn_id)
+            entity_input["internal_vnet_id"] = self.find_vxlan_ids(entity_input["internal_vnet_id"], tn_descriptor, report_directory, tn_id)
         elif entity_type == "ueransim":
-            entity_public["one_component_networks"] = self.find_vxlan_ids(entity_public["one_component_networks"], tn_descriptor, report_directory, tn_id)
-        return entity_public
+            entity_input["one_component_networks"] = self.find_vxlan_ids(entity_input["one_component_networks"], tn_descriptor, report_directory, tn_id)
+        return entity_input
 
     def create_entity_temp_file(self, entity_name, entity_data, tn_descriptor, report_directory, tn_id):
         """Create temporary files for each entity that is deployed in the pipeline and returns the path to the file"""
-        log_handler.info(f"Creating a temporary file with the private and public part for the '{entity_name}' entity. It is required to send it to the pipeline")
+        log_handler.info(f"Creating a temporary file with the private and input part for the '{entity_name}' entity. It is required to send it to the pipeline")
         with NamedTemporaryFile(delete=False, dir=TEMP_FILES_PATH, suffix=".yaml", mode='w') as entity_temp_file:
-            entity_public = self._add_private_entity_parameters(entity_name, entity_data, tn_descriptor, report_directory, tn_id)
-            dump(entity_public, entity_temp_file, default_flow_style=False)
+            entity_input = self._add_private_entity_parameters(entity_name, entity_data, tn_descriptor, report_directory, tn_id)
+            dump(entity_input, entity_temp_file, default_flow_style=False)
         return entity_temp_file.name
