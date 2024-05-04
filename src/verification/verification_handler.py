b172b3544c75e1750295859dbe5dc35a9b7376ac
CarlosAndreo <c.andreo@uma.es>
2024-05-04 10:57:25 +0200

    Update with mongo_handler in constructor

--- src/verification/verification_handler.py
+++ src/verification/verification_handler.py
@@ -1,17 +1,16 @@
 from datetime import datetime, timezone
 
 from src.logs.log_handler import log_handler
-from src.database.mongo_handler import MongoHandler
 
 class VerificationHandler:
 
-    def __init__(self, new_account_email, verification_token):
+    def __init__(self, mongo_handler=None, new_account_email=None, verification_token=None):
         """Constructor"""
+        self.mongo_handler = mongo_handler
         self.new_account_email = new_account_email
         self.verification_token = verification_token
         self.creation_date = datetime.now(timezone.utc).strftime("%Y-%m-%d %H:%M:%S")
-        self.mongo_client = MongoHandler()
-    
+
     def add_verification_token(self):
         """Add verification token to database"""
         log_handler.info(f"Add new account to the TNLCM")
@@ -20,19 +19,19 @@ class VerificationHandler:
             "verification_token": self.verification_token,
             "creation_date": self.creation_date
         }
-        self.mongo_client.insert_data("verification_tokens", verification_token_doc)
-    
+        self.mongo_handler.insert_data("verification_tokens", verification_token_doc)
+
     def update_verification_token(self):
         """Update verification token"""
         query = {"new_account_email": self.new_account_email}
         projection = {"$set": {"verification_token": self.verification_token, "creation_date": self.creation_date}}
         log_handler.info(f"Update the verification token associated with the '{self.new_account_email}' email account in the database 'verification_tokens' collection")
-        self.mongo_client.update_data(collection_name="verification_tokens", query=query, projection=projection)
+        self.mongo_handler.update_data(collection_name="verification_tokens", query=query, projection=projection)
 
     def get_verification_token(self):
         """Return verification token associated an email of user"""
         log_handler.info(f"Check the verification token associated with the '{self.new_account_email}' email account in the database 'verification_tokens' collection")
         query = {"new_account_email": self.new_account_email, "verification_token": self.verification_token}
         projection = {"_id": 0, "verification_token": 1}
-        verification_token = self.mongo_client.find_data(collection_name="verification_tokens", query=query, projection=projection)
+        verification_token = self.mongo_handler.find_data(collection_name="verification_tokens", query=query, projection=projection)
         return verification_token
