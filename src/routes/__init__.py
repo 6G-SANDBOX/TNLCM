c088623c20f0ae42e63b2674dc0ab7dd82e40dde
CarlosAndreo <c.andreo@uma.es>
2024-05-04 11:02:18 +0200

    Add sixgsandbox_sites namespace

--- src/routes/__init__.py
+++ src/routes/__init__.py
@@ -1,5 +1,6 @@
 from .callback import callback_namespace
-from .trial_network import trial_network_namespace
 from .sixglibrary import sixglibrary_namespace
+from .sixgsandbox_sites import sixgsandbox_sites_namespace
+from .trial_network import trial_network_namespace
 from .users import users_namespace
 from .verification import verification_namespace
