74855d8227b69a5496355cd592ace51db4d4e79f
CarlosAndreo <c.andreo@uma.es>
2024-05-04 11:00:43 +0200

    Add logs and update with generic exceptions

--- src/trial_network/trial_network_descriptor.py
+++ src/trial_network/trial_network_descriptor.py
@@ -3,11 +3,11 @@ from json import dumps
 from yaml import safe_load, YAMLError
 
 from src.logs.log_handler import log_handler
-from src.exceptions.exceptions_handler import TrialNetworkDescriptorInvalidExtensionError, TrialNetworkDescriptorEmptyError, TrialNetworkDescriptorInvalidContentError, TrialNetworkEntityNotInDescriptorError
+from src.exceptions.exceptions_handler import InvalidFileExtensionError, InvalidContentError, TrialNetworkDescriptorEmptyError, TrialNetworkEntityNotInDescriptorError
 
 class TrialNetworkDescriptorHandler:
 
-    def __init__(self, current_user, descriptor):
+    def __init__(self, current_user=None, descriptor=None):
         """Constructor"""
         self.current_user = current_user
         self.descriptor = descriptor
@@ -15,18 +15,20 @@ class TrialNetworkDescriptorHandler:
     def check_descriptor(self):
         """Check that the descriptor file is well constructed and that its extension is yaml or yml"""
         try:
+            log_handler.info("Check if descriptor extension is yml or yaml file")
             filename = secure_filename(self.descriptor.filename)
             if '.' in filename and filename.split('.')[-1].lower() in ["yml", "yaml"]:
                 self.descriptor = safe_load(self.descriptor.stream)
             else:
-                raise TrialNetworkDescriptorInvalidExtensionError("Invalid descriptor format. Only 'yml' or 'yaml' files will be further processed", 422)
+                raise InvalidFileExtensionError("Invalid descriptor format. Only 'yml' or 'yaml' files will be further processed", 422)
             if self.descriptor["trial_network"] is None:
                 raise TrialNetworkDescriptorEmptyError("Trial network descriptor empty", 400)
         except YAMLError:
-            raise TrialNetworkDescriptorInvalidContentError("Descriptor content not properly parsed", 422)
+            raise InvalidContentError("Descriptor content not properly parsed", 422)
 
     def add_entity_mandatory_tn_vxlan(self):
         """Add the entity vxlan to the descriptor (mandatory)"""
+        log_handler.info("Add mandatory tn_vxlan to descriptor")
         if not self._is_entity_descriptor("mandatory_tn_vxlan"):
             entity_data = {
                 "input": {
@@ -37,6 +39,7 @@ class TrialNetworkDescriptorHandler:
 
     def add_entity_mandatory_tn_bastion(self):
         """Add the entity bastion to the descriptor (mandatory)"""
+        log_handler.info("Add mandatory tn_bastion to descriptor")
         if not self._is_entity_descriptor("mandatory_tn_bastion"):
             entity_data = {
                 "needs": ["mandatory_tn_vxlan"],
@@ -58,8 +61,8 @@ class TrialNetworkDescriptorHandler:
         self.descriptor["trial_network"][entity_name] = entity_data
 
     def sort_descriptor(self):
-        """Recursive function that returns the raw descriptor and a new descriptor sorted according to dependencies"""
-        log_handler.info("Start the order of the entities")
+        """Recursive method that returns the raw descriptor and a new descriptor sorted according to dependencies (needs)"""
+        log_handler.info("Start order of the entities of the descriptor")
         entities = self.descriptor["trial_network"]
         ordered_entities = {}
 
@@ -76,5 +79,5 @@ class TrialNetworkDescriptorHandler:
         for entity in entities:
             dfs(entity)
         
-        log_handler.info("End the order of the entities")
+        log_handler.info("End order of the entities of the descriptor")
         return dumps(self.descriptor), dumps({"trial_network": ordered_entities})
