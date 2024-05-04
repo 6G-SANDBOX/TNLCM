ef5d775a5d71ff129d23bfe1a5e912e4f60e4142
CarlosAndreo <c.andreo@uma.es>
2024-05-04 10:55:26 +0200

    Update if else in main

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from dotenv import load_dotenv
 
 from src.logs.log_handler import log_handler
 from src.verification.mail import init_mail
-from src.routes import trial_network_namespace, callback_namespace, sixglibrary_namespace, users_namespace, verification_namespace
+from src.routes import callback_namespace, sixglibrary_namespace, sixgsandbox_sites_namespace, trial_network_namespace, users_namespace, verification_namespace
 
 app = Flask(__name__)
 CORS(app)
@@ -17,10 +17,10 @@ JWTManager(app)
 
 load_dotenv()
 flask_env = os.getenv("FLASK_ENV").upper()
-if flask_env == "DEVELOPMENT":
-    app.config.from_object("config.DevelopmentConfig")
-elif flask_env == "PRODUCTION":
+if flask_env == "PRODUCTION":
     app.config.from_object("config.ProductionConfig")
+elif flask_env == "DEVELOPMENT":
+    app.config.from_object("config.DevelopmentConfig")
 else:
     app.config.from_object("config.TestingConfig")
 
@@ -38,18 +38,17 @@ api = Api(
     # doc=False
 )
 
+api.add_namespace(callback_namespace, path="/tnlcm/callback")
+api.add_namespace(sixglibrary_namespace, path="/tnlcm/6glibrary")
+api.add_namespace(sixgsandbox_sites_namespace, path="/tnlcm/6gsandbox_sites")
+api.add_namespace(trial_network_namespace, path="/tnlcm/trial_network")
 api.add_namespace(users_namespace, path="/tnlcm/user")
 api.add_namespace(verification_namespace, path="/tnlcm/verification")
-api.add_namespace(trial_network_namespace, path="/tnlcm/trial_network")
-api.add_namespace(sixglibrary_namespace, path="/tnlcm/6glibrary")
-api.add_namespace(callback_namespace, path="/tnlcm/callback")
 
 log_handler.info("Start Server Trial Network Life Cycle Manager (TNLCM) on http://0.0.0.0:5000")
 
 if __name__ == "__main__":
-    if flask_env == "DEVELOPMENT":
-        app.run(host="0.0.0.0", port=5000)
-    elif flask_env == "PRODUCTION":
+    if flask_env == "PRODUCTION":
         serve(app, host="0.0.0.0", port=5000)
     else:
         app.run(host="0.0.0.0", port=5000)
