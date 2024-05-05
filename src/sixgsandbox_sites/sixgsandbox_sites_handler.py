ef7f23700f1374a8ffb6777a82580d9185166134
CarlosAndreo <c.andreo@uma.es>
2024-05-05 16:54:48 +0200

    Add logs and fix raise

--- src/sixgsandbox_sites/sixgsandbox_sites_handler.py
+++ src/sixgsandbox_sites/sixgsandbox_sites_handler.py
@@ -2,9 +2,9 @@ import os
 
 from yaml import safe_load, YAMLError
 
-from src.logs.log_handler import LogHandler
+from src.logs.log_handler import log_handler
 from src.repository.repository_handler import RepositoryHandler
-from src.exceptions.exceptions_handler import VariablesNotDefinedInEnvError, KeyNotFoundError, InvalidContentError
+from src.exceptions.exceptions_handler import VariablesNotDefinedInEnvError, KeyNotFoundError, InvalidContentFileError, CustomFileNotFoundError
 
 SIXGSANDBOX_SITES_DIRECTORY = os.path.join(os.getcwd(), "src", "sixgsandbox_sites")
 
@@ -27,27 +27,33 @@ class SixGSandboxSitesHandler():
     def extract_site_default_network_id(self, jenkins_deployment_site):
         """Return a list with the id of the site_default_network_id"""
         site_file = os.path.join(self.git_6gsandbox_sites_local_directory, ".sites", jenkins_deployment_site, "values.yaml")
+        log_handler.info(f"Extract 'site_default_network_id' from '{site_file}'")
         if os.path.exists(site_file):
             with open(site_file, "rt", encoding="utf-8") as f:
                 try:
                     data = safe_load(f)
                 except YAMLError:
-                    raise InvalidContentError("Descriptor content not properly parsed", 422)
-                if "site_default_network_id" in data:
+                    raise InvalidContentFileError(f"File '{site_file}' is not parsed correctly", 422)
+                if "site_default_network_id" in data.keys():
                     return data["site_default_network_id"]
                 else:
                     raise KeyNotFoundError(f"Key 'site_default_network_id' is missing in the file located in the path '{site_file}'", 400)
+        else:
+            raise CustomFileNotFoundError(f"File '{site_file}' not found", 404)
 
     def extract_site_public_network_id(self, jenkins_deployment_site):
         """Return a list with the id of the site_public_network_id"""
         site_file = os.path.join(self.git_6gsandbox_sites_local_directory, ".sites", jenkins_deployment_site, "values.yaml")
+        log_handler.info(f"Extract 'site_public_network_id' from '{site_file}'")
         if os.path.exists(site_file):
             with open(site_file, "rt", encoding="utf-8") as f:
                 try:
                     data = safe_load(f)
                 except YAMLError:
-                    raise InvalidContentError("Descriptor content not properly parsed", 422)
-                if "site_public_network_id" in data:
+                    raise InvalidContentFileError(f"File '{site_file}' is not parsed correctly", 422)
+                if "site_public_network_id" in data.keys():
                     return data["site_public_network_id"]
                 else:
                     raise KeyNotFoundError(f"Key 'site_public_network_id' is missing in the file located in the path '{site_file}'", 400)
+        else:
+            raise CustomFileNotFoundError(f"File '{site_file}' not found", 404)
