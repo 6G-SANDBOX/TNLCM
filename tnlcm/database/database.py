3a5bfc6c2cce3bdc42abd2f596703f946daae4f2
Carlos Andreo <c.andreo@uma.es>
2024-05-08 15:55:54 +0200

    Try to fix socket warning

--- tnlcm/database/database.py
+++ tnlcm/database/database.py
@@ -1,6 +1,7 @@
-from mongoengine import connect
+from mongoengine import connect, disconnect
 
 def init_db(app):
+    # disconnect(alias="tnlcm-database-alias")
     connect(
         alias="tnlcm-database-alias",
         host=app.config["MONGO_URI"]
