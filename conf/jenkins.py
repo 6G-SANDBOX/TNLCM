bfee459593c1e894f7725c0d79bf5e90372d3b3b
Carlos Andreo <c.andreo@uma.es>
2024-05-20 19:22:22 +0200

    Delete JENKINS_PIPELINE_FOLDER and JENKINS_PIPELINE_NAME

--- conf/jenkins.py
+++ conf/jenkins.py
@@ -15,8 +15,6 @@ class JenkinsSettings:
     JENKINS_USERNAME = os.getenv("JENKINS_USERNAME")
     JENKINS_PASSWORD = os.getenv("JENKINS_PASSWORD")
     JENKINS_TOKEN = os.getenv("JENKINS_TOKEN")
-    JENKINS_PIPELINE_FOLDER = os.getenv("JENKINS_PIPELINE_FOLDER")
-    JENKINS_PIPELINE_NAME = os.getenv("JENKINS_PIPELINE_NAME")
     JENKINS_DEPLOYMENT_SITE = os.getenv("JENKINS_DEPLOYMENT_SITE")
     TNLCM_CALLBACK = os.getenv("TNLCM_CALLBACK")
     missing_variables = []
@@ -30,8 +28,6 @@ class JenkinsSettings:
         missing_variables.append("JENKINS_PASSWORD")
     if not JENKINS_TOKEN:
         missing_variables.append("JENKINS_TOKEN")
-    if not JENKINS_PIPELINE_NAME:
-        missing_variables.append("JENKINS_PIPELINE_NAME")
     if not JENKINS_DEPLOYMENT_SITE:
         missing_variables.append("JENKINS_DEPLOYMENT_SITE")
     if not TNLCM_CALLBACK:
@@ -42,4 +38,3 @@ class JenkinsSettings:
     JENKINS_DEPLOYMENT_SITE = JENKINS_DEPLOYMENT_SITE.lower()
     if JENKINS_DEPLOYMENT_SITE not in JENKINS_DEPLOYMENT_SITES:
         raise InvalidEnvVariableValueError(f"The value of the variable 'JENKINS_DEPLOYMENT_SITE' should be {', '.join(JENKINS_DEPLOYMENT_SITES)} in the .env file")
-    JENKINS_PIPELINE = JENKINS_PIPELINE_NAME if not JENKINS_PIPELINE_FOLDER else f"{JENKINS_PIPELINE_FOLDER}/{JENKINS_PIPELINE_NAME}"
