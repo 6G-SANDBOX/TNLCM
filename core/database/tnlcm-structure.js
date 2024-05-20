b88f223a01462ded53725cbf7c117ed20db81960
Carlos Andreo <c.andreo@uma.es>
2024-05-20 23:14:21 +0200

    Add function to load database name from .env file

--- core/database/tnlcm-structure.js
+++ core/database/tnlcm-structure.js
@@ -1,4 +1,20 @@
-var dbName = 'tnlcm-database';
+const fs = require('fs');
+const path = require('path');
+
+function loadDatabaseName() {
+    const envPath = path.resolve(__dirname, '..', '..', '.env');
+    const envContents = fs.readFileSync(envPath, 'utf-8');
+    const envVars = envContents.split(/\r?\n/);
+
+    for (const line of envVars) {
+        const [key, value] = line.split('=');
+        if (key === "MONGO_DATABASE") {
+            return value.toString().replace(/"/g, '');
+        }
+    }
+}
+
+var dbName = loadDatabaseName();
 var trialNetworks = 'trial_networks';
 var trialNetworksTemplates = 'trial_networks_templates';
 var users = 'users';
