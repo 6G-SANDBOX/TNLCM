64e89c01eadd89008f976043b905e02a86753504
Carlos Andreo <c.andreo@uma.es>
2024-05-20 13:04:45 +0200

    Add jenkins ip, port and delete jenkins url

--- conf/jenkins.py
+++ conf/jenkins.py
@@ -10,7 +10,8 @@ class JenkinsSettings:
     
     log_handler.info("Load Jenkins configuration")
 
-    JENKINS_URL = os.getenv("JENKINS_URL")
+    JENKINS_IP = os.getenv("JENKINS_IP")
+    JENKINS_PORT = os.getenv("JENKINS_PORT")
     JENKINS_USERNAME = os.getenv("JENKINS_USERNAME")
     JENKINS_PASSWORD = os.getenv("JENKINS_PASSWORD")
     JENKINS_TOKEN = os.getenv("JENKINS_TOKEN")
@@ -19,8 +20,10 @@ class JenkinsSettings:
     JENKINS_DEPLOYMENT_SITE = os.getenv("JENKINS_DEPLOYMENT_SITE")
     TNLCM_CALLBACK = os.getenv("TNLCM_CALLBACK")
     missing_variables = []
-    if not JENKINS_URL:
-        missing_variables.append("JENKINS_URL")
+    if not JENKINS_IP:
+        missing_variables.append("JENKINS_IP")
+    if not JENKINS_PORT:
+        missing_variables.append("JENKINS_PORT")
     if not JENKINS_USERNAME:
         missing_variables.append("JENKINS_USERNAME")
     if not JENKINS_PASSWORD:
@@ -35,6 +38,7 @@ class JenkinsSettings:
         missing_variables.append("TNLCM_CALLBACK")
     if missing_variables:
         raise UndefinedEnvVariableError(missing_variables)
+    JENKINS_URL = f"http://{JENKINS_IP}:{JENKINS_PORT}"
     JENKINS_DEPLOYMENT_SITE = JENKINS_DEPLOYMENT_SITE.lower()
     if JENKINS_DEPLOYMENT_SITE not in JENKINS_DEPLOYMENT_SITES:
         raise InvalidEnvVariableValueError(f"The value of the variable 'JENKINS_DEPLOYMENT_SITE' should be {', '.join(JENKINS_DEPLOYMENT_SITES)} in the .env file")
