7ca95cda62e2cb2cbb6d4cf766fb8f96ecc76c01
Carlos Andreo <c.andreo@uma.es>
2024-05-29 17:21:32 +0200

    Delete get vnet ids and add rbanch, commit and tag option

--- core/sixg_sandbox_sites/sixg_sandbox_sites_handler.py
+++ core/sixg_sandbox_sites/sixg_sandbox_sites_handler.py
@@ -1,60 +1,40 @@
 import os
 
-from yaml import safe_load, YAMLError
-
-from core.logs.log_handler import log_handler
 from conf import SixGSandboxSitesSettings
 from core.repository.repository_handler import RepositoryHandler
-from core.exceptions.exceptions_handler import KeyNotFoundError, InvalidContentFileError, CustomFileNotFoundError
+from core.exceptions.exceptions_handler import GitRequiredFieldError, SixGSandboxSitesInvalidSiteError
 
 SIXG_SANDBOX_SITES_DIRECTORY = os.path.dirname(os.path.abspath(__file__))
 
 class SixGSandboxSitesHandler():
 
-    def __init__(self):
+    def __init__(self, branch=None, commit_id=None, tag=None, deployment_site=None):
         """Constructor"""
         self.github_6g_sandbox_sites_https_url = SixGSandboxSitesSettings.GITHUB_6G_SANDBOX_SITES_HTTPS_URL
         self.github_6g_sandbox_sites_repository_name = SixGSandboxSitesSettings.GITHUB_6G_SANDBOX_SITES_REPOSITORY_NAME
-        self.github_6g_sandbox_sites_branch = SixGSandboxSitesSettings.GITHUB_6G_SANDBOX_SITES_BRANCH
         self.github_6g_sandbox_sites_local_directory = os.path.join(SIXG_SANDBOX_SITES_DIRECTORY, self.github_6g_sandbox_sites_repository_name)
-        self.repository_handler = RepositoryHandler(github_https_url=self.github_6g_sandbox_sites_https_url, github_repository_name=self.github_6g_sandbox_sites_repository_name, github_branch=self.github_6g_sandbox_sites_branch, github_local_directory=self.github_6g_sandbox_sites_local_directory)
+        self.github_6g_sandbox_sites_branch = None
+        self.github_6g_sandbox_sites_commit_id = None
+        self.github_6g_sandbox_sites_tag = None
+        if branch and not commit_id and not tag:
+            self.github_6g_sandbox_sites_branch = branch
+        elif not branch and commit_id and not tag:
+            self.github_6g_sandbox_sites_commit_id = commit_id
+        elif not branch and not commit_id and tag:
+            self.github_6g_sandbox_sites_tag = tag
+        elif not branch and not commit_id and not tag:
+            self.github_6g_sandbox_sites_branch = SixGSandboxSitesSettings.GITHUB_6G_SANDBOX_SITES_BRANCH
+        else:
+            raise GitRequiredFieldError("Only one field is required. Either branch, commit_id or tag", 400)
+        if deployment_site not in self.get_sites():
+            raise SixGSandboxSitesInvalidSiteError(f"The 'site' should be one: {', '.join(self.get_sites())}", 400)
+        self.deployment_site = deployment_site
+        self.repository_handler = RepositoryHandler(github_https_url=self.github_6g_sandbox_sites_https_url, github_repository_name=self.github_6g_sandbox_sites_repository_name, github_branch=self.github_6g_sandbox_sites_branch, github_commit_id=self.github_6g_sandbox_sites_commit_id, github_tag=self.github_6g_sandbox_sites_tag, github_local_directory=self.github_6g_sandbox_sites_local_directory)
 
     def git_clone_6g_sandbox_sites(self):
         """Clone 6G-Sandbox-Sites"""
         self.repository_handler.git_clone_repository()
 
-    def get_site_default_network_id(self, jenkins_deployment_site):
-        """Return site default network id"""
-        site_file = os.path.join(self.github_6g_sandbox_sites_local_directory, ".sites", jenkins_deployment_site, "values.yaml")
-        log_handler.info(f"Get site 'default' network id from '{site_file}'")
-        if os.path.exists(site_file):
-            with open(site_file, "rt", encoding="utf-8") as f:
-                try:
-                    data = safe_load(f)
-                except YAMLError:
-                    raise InvalidContentFileError(f"File '{site_file}' is not parsed correctly", 422)
-            if "site_networks_id" not in data:
-                raise KeyNotFoundError(f"Key 'site_networks_id' is missing in the file located in the path '{site_file}'", 404)
-            if "default" not in data["site_networks_id"]:
-                raise KeyNotFoundError(f"Key 'default' is missing into 'site_networks_id' in the file located in the path '{site_file}'", 404)
-            return data["site_networks_id"]["default"]
-        else:
-            raise CustomFileNotFoundError(f"File '{site_file}' not found", 404)
-
-    def get_site_public_network_id(self, jenkins_deployment_site):
-        """Return site public network id"""
-        site_file = os.path.join(self.github_6g_sandbox_sites_local_directory, ".sites", jenkins_deployment_site, "values.yaml")
-        log_handler.info(f"Get site 'public' network id from '{site_file}'")
-        if os.path.exists(site_file):
-            with open(site_file, "rt", encoding="utf-8") as f:
-                try:
-                    data = safe_load(f)
-                except YAMLError:
-                    raise InvalidContentFileError(f"File '{site_file}' is not parsed correctly", 422)
-            if "site_networks_id" not in data:
-                raise KeyNotFoundError(f"Key 'site_networks_id' is missing in the file located in the path '{site_file}'", 404)
-            if "public" not in data["site_networks_id"]:
-                raise KeyNotFoundError(f"Key 'public' is missing into 'site_networks_id' in the file located in the path '{site_file}'", 404)
-            return data["site_networks_id"]["public"]
-        else:
-            raise CustomFileNotFoundError(f"File '{site_file}' not found", 404)
+    def get_sites(self):
+        """Return sites available to deploy trial networks"""
+        return os.listdir(os.path.join(self.github_6g_sandbox_sites_local_directory, ".sites"))
