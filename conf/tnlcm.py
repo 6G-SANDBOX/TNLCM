d134ece9d8c2ddd42eb5ca4a56ff0b7604ae6718
Carlos Andreo <c.andreo@uma.es>
2024-06-02 22:20:24 +0200

    Add check Python version

--- conf/tnlcm.py
+++ conf/tnlcm.py
@@ -1,9 +1,20 @@
+import sys
+
 from core.logs.log_handler import log_handler
+from core.exceptions.exceptions_handler import InvalidPythonVersionError
+
+PYTHON_MIN_REQUIRED = (3, 12, 3)
 
 class TnlcmSettings:
     """TNLCM Settings"""
 
     log_handler.info("Load TNLCM configuration")
+
+    current_python_version = sys.version_info[:3]
+    if current_python_version < PYTHON_MIN_REQUIRED:
+        raise InvalidPythonVersionError(f"Python {PYTHON_MIN_REQUIRED} or higher is required. Current version: {current_python_version}", 404)
+    
+    log_handler.info(f"Current Python version: {current_python_version}")
     TITLE = "Trial Network Life Cycle Manager - TNLCM"
     VERSION = "0.2.0"
     DESCRIPTION = ("""
