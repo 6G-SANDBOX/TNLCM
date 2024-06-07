215c0a3e6e2437d6b801141ad2f5c4f7e9d97e25
Carlos Andreo <c.andreo@uma.es>
2024-06-07 15:05:54 +0200

    Fix git pull and git checkout

--- core/repository/repository_handler.py
+++ core/repository/repository_handler.py
@@ -22,9 +22,9 @@ class RepositoryHandler:
             if os.path.exists(os.path.join(self.github_local_directory, ".git")):
                 try:
                     self.repo = Repo(self.github_local_directory)
+                    self._git_checkout(reference="main")
                     self._git_pull()
                     self._git_checkout()
-                    self._git_pull()
                 except InvalidGitRepositoryError:
                     raise GitCloneError(f"The '{self.github_local_directory}' directory is not a GitHub repository", 500)
             else:
@@ -43,13 +43,17 @@ class RepositoryHandler:
         except InvalidGitRepositoryError:
             raise GitCloneError(f"Cannot clone because the '{self.github_https_url}' url is not a GitHub repository", 500)
 
-    def _git_checkout(self):
+    def _git_checkout(self, reference=None):
         """Checkout to branch, commit or tag"""
         if not self.repo:
             raise GitCloneError(f"Clone '{self.github_repository_name}' repository first", 500)
         try:
-            log_handler.info(f"Apply checkout to '{self.github_reference}' reference (branch, commit or tag) of '{self.github_repository_name}' repository")
-            self.repo.git.checkout(self.github_reference, "--")
+            if not reference:
+                log_handler.info(f"Apply checkout to '{self.github_reference}' reference (branch, commit or tag) of '{self.github_repository_name}' repository")
+                self.repo.git.checkout(self.github_reference, "--")
+            else:
+                log_handler.info(f"Apply checkout to '{reference}' reference (branch, commit or tag) of '{self.github_repository_name}' repository")
+                self.repo.git.checkout(reference, "--")
         except GitCommandError:
             raise GitCheckoutError(f"Reference '{self.github_reference}' is not in '{self.github_repository_name}' repository", 404)
 
@@ -58,6 +62,10 @@ class RepositoryHandler:
         if not self.repo.head.is_detached:
             log_handler.info(f"Pull is executed in '{self.github_repository_name}' repository located in '{self.github_local_directory}' folder")
             self.repo.remotes.origin.pull()
+
+    def _get_default_branch(self):
+        """Return the default branch of the repository"""
+        return self.repo.remotes.origin.refs[self.repo.head.reference].name.replace("origin/", "")
 
     def get_tags(self):
         """Return repository tags"""
