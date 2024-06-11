5e0f4ad21841ae6ca051e5281dc9069543bd8199
Carlos Andreo <c.andreo@uma.es>
2024-06-11 10:24:23 +0200

    Add release resource manager

--- core/resource_manager/resource_manager.py
+++ core/resource_manager/resource_manager.py
@@ -3,7 +3,7 @@ from core.exceptions.exceptions_handler import NoResourcesAvailable
 
 class ResourceManagerHandler():
 
-    def __init__(self, trial_network, sixg_sandbox_sites_handler):
+    def __init__(self, trial_network=None, sixg_sandbox_sites_handler=None):
         self.trial_network = trial_network
         self.sixg_sandbox_sites_handler = sixg_sandbox_sites_handler
 
@@ -31,7 +31,7 @@ class ResourceManagerHandler():
 
     def apply_resource_manager(self):
         """Apply resource manager to check availability resource"""
-        tn_descriptor = self.trial_network.json_to_descriptor(self.trial_network.tn_deployed_descriptor)["trial_network"]
+        tn_descriptor = self.trial_network.json_to_descriptor(self.trial_network.tn_sorted_descriptor)["trial_network"]
         for _, entity_data in tn_descriptor.items():
             component_type = entity_data["type"]
             sixg_sandbox_sites_component_quantity, sixg_sandbox_sites_component_ttl = self._sixg_sandbox_sites_component_resources(component_type)
@@ -41,12 +41,16 @@ class ResourceManagerHandler():
                     raise NoResourcesAvailable(f"Component '{component_type}' is not available on the '{self.trial_network.deployment_site}' platform", 400)
                 tnlcm_component_resources = ResourceManagerModel.objects(site=self.trial_network.deployment_site, component=component_type).first()
                 if not tnlcm_component_resources:
-                    tnlcm_component_resources = ResourceManagerModel(site=self.trial_network.deployment_site, tn_ids=[self.trial_network.tn_id], component=component_type, quantity=1, ttl=sixg_sandbox_sites_component_ttl)
+                    tnlcm_component_resources = ResourceManagerModel(site=self.trial_network.deployment_site, component=component_type, quantity=1, ttl=sixg_sandbox_sites_component_ttl)
                 else:
-                    tnlcm_component_resources.tn_ids.append(self.trial_network.tn_id)
                     tnlcm_component_resources.quantity += 1
                 tnlcm_component_resources.save()
     
-    def release_resource_manager(self): # TODO
+    def release_resource_manager(self):
         """Release resources when destroy or suspend trial network"""
-        pass
+        tn_descriptor = self.trial_network.json_to_descriptor(self.trial_network.tn_sorted_descriptor)["trial_network"]
+        for _, entity_data in tn_descriptor.items():
+            component_type = entity_data["type"]
+            tnlcm_component_resources = ResourceManagerModel.objects(site=self.trial_network.deployment_site, component=component_type).first()
+            tnlcm_component_resources.quantity -= 1
+            tnlcm_component_resources.save()
