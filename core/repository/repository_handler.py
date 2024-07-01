c60fea62dbf50790a21558fca746f9c7423b88cb
Carlos Andreo <c.andreo@uma.es>
2024-07-01 19:59:33 +0000

    Fix clean repository

--- core/repository/repository_handler.py
+++ core/repository/repository_handler.py
@@ -46,7 +46,7 @@ class RepositoryHandler:
             if os.path.exists(os.path.join(self.github_local_directory, ".git")):
                 try:
                     self.repo = Repo(self.github_local_directory)
-                    self.repo.git.clean("df")
+                    self.repo.git.clean("-fd")
                 except InvalidGitRepositoryError:
                     raise GitCloneError(f"The '{self.github_local_directory}' directory is not a GitHub repository", 500)
             else:
