2b5b8610d09d76501333a9462a4b35cde3a15f43
CarlosAndreo <c.andreo@uma.es>
2024-10-09 14:25:55 +0000

    Add dev .env

--- conf/__init__.py
+++ conf/__init__.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 from core.logs.log_handler import log_handler
 
 dotenv_path = os.path.join(os.getcwd(), ".env")
+dotenv_path_dev = os.path.join(os.getcwd(), ".env.dev")
 log_handler.info(f"Loading the '{dotenv_path}' file of the project")
 load_dotenv(dotenv_path=dotenv_path)
 
