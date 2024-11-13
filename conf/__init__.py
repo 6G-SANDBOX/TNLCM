fbbe502d0fecf36168efb639c9464bf42f661998
CarlosAndreo <c.andreo@uma.es>
2024-11-13 11:59:40 +0000

    Change: add pyfiglet for ASCII art output and print TNLCM title in init

--- conf/__init__.py
+++ conf/__init__.py
@@ -1,6 +1,11 @@
 import os
 
 from dotenv import load_dotenv
+from pyfiglet import figlet_format
+
+print(figlet_format("Trial Network Lifecycle Management", font="small"))
+print(figlet_format("TNLCM", font="small"))
+# TODO: add subtitulo, tamanio de la letra
 
 dotenv_path = os.path.join(os.getcwd(), ".env")
 # dotenv_path = os.path.join(os.getcwd(), ".env.dev")
