6f5f7d0a263d0be3d00a5e603744f2e0634361ff
CarlosAndreo <c.andreo@uma.es>
2025-02-04 10:37:32 +0000

    fix: revert change

--- core/database/tnlcm-structure.js
+++ core/database/tnlcm-structure.js
@@ -1,5 +1,5 @@
 const crypto = require("crypto");
-require("/root/.bun/install/global/node_modules/dotenv").config({ 
+require("/opt/yarn_global/node_modules/dotenv").config({ 
     path: require("path").resolve(__dirname, "../../.env")
 });
 
