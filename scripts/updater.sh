71f6f49c98ed44dcd8b6af3c9520f60b5f3f9eed
CarlosAndreo <c.andreo@uma.es>
2025-02-10 16:45:01 +0000

    add: updater from 0.4.4 to 0.4.5

--- scripts/updater.sh
+++ scripts/updater.sh
@@ -3,14 +3,111 @@
 export DEBIAN_FRONTEND=noninteractive
 
 BACKEND_PATH="/opt/TNLCM_BACKEND"
-TNLCM_ENV_FILE=${BACKEND_PATH}/.env
+BACKEND_DOTENV_FILE="${BACKEND_PATH}/.env"
+BACKEND_VENV_PATH="${BACKEND_PATH}/.venv"
+UV_PATH="/opt/uv"
+UV_BIN="${UV_PATH}/uv"
+MONGO_DATABASE="tnlcm-database"
+TRIAL_NETWORK_COLLECTION="trial_network"
+MONGO_EXPRESS_PATH="/opt/mongo-express"
 
-TNLCM_VERSION=$(grep -oP 'version = "\K[^"]+' ${BACKEND_PATH}/pyproject.toml)
+git -C ${BACKEND_PATH} pull
+git -C ${BACKEND_PATH} fetch --tags
 
-if [[ $(echo "${TNLCM_VERSION} <= 0.4.4" | bc) -eq 1 ]]; then
-    echo "TNLCM version is ${TNLCM_VERSION}"
-    echo "TNLCM version must be greater than 0.4.4"
-    exit 1
+TNLCM_VERSIONS=($(git -C ${BACKEND_PATH} tag | sort -V | awk '$0 >= "v0.4.4"'))
+PS3="Select the version you want to upgrade to: "
+select TARGET_VERSION in "${TNLCM_VERSIONS[@]}"; do
+    if [[ -n "${TARGET_VERSION}" ]]; then
+        break
+    else
+        echo "Invalid selection. Please try again"
+    fi
+done
+
+TARGET_VERSION=${TARGET_VERSION#v}
+
+CURRENT_VERSION=$(grep -oP 'version = "\K[^"]+' ${BACKEND_PATH}/pyproject.toml)
+
+if [[ "$(printf "%s\n%s" "${CURRENT_VERSION}" "${TARGET_VERSION}" | sort -V | head -n 1)" == "${TARGET_VERSION}" ]]; then
+    echo "You are already on version ${CURRENT_VERSION} or higher"
+    exit 0
 fi
 
-echo "TNLCM version is ${TNLCM_VERSION}"
+echo "Starting upgrade from ${CURRENT_VERSION} to ${TARGET_VERSION}..."
+
+if [[ "${CURRENT_VERSION}" == "0.4.4" && "${TARGET_VERSION}" == "0.4.5" ]]; then
+
+    MONGO_EXPRESS_VERSION="v1.1.0-rc-3"
+    POETRY_PATH="/opt/poetry"
+
+    rm -r ${BACKEND_VENV_PATH}
+
+    git checkout v${TARGET_VERSION}
+
+    curl -LsSf https://astral.sh/uv/install.sh | env UV_INSTALL_DIR=${UV_PATH} sh
+    ${UV_BIN} --directory ${BACKEND_PATH} sync
+
+    sed -i 's/^GITHUB_6G_LIBRARY_HTTPS_URL=/LIBRARY_HTTPS_URL=/' "${BACKEND_DOTENV_FILE}"
+    sed -i 's/^GITHUB_6G_LIBRARY_BRANCH=/LIBRARY_BRANCH=/' "${BACKEND_DOTENV_FILE}"
+    sed -i 's/^GITHUB_6G_LIBRARY_REPOSITORY_NAME=/LIBRARY_REPOSITORY_NAME=/' "${BACKEND_DOTENV_FILE}"
+    sed -i 's/^GITHUB_6G_SANDBOX_SITES_HTTPS_URL=/SITES_HTTPS_URL=/' "${BACKEND_DOTENV_FILE}"
+    sed -i 's/^GITHUB_6G_SANDBOX_SITES_REPOSITORY_NAME=/SITES_REPOSITORY_NAME=/' "${BACKEND_DOTENV_FILE}"
+    sed -i '/^GITHUB_6G_SANDBOX_SITES_BRANCH=/d' "${BACKEND_DOTENV_FILE}"
+
+    cat <<EOF > /etc/systemd/system/tnlcm-backend.service
+[Unit]
+Description=TNLCM Backend
+After=network.target
+
+[Service]
+Type=simple
+WorkingDirectory=${BACKEND_PATH}/
+ExecStart=/bin/bash -c '${UV_BIN} run gunicorn -c conf/gunicorn_conf.py'
+Restart=always
+User=root
+Group=root
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+    systemctl daemon-reload
+    systemctl restart tnlcm-backend.service
+
+    mongosh --eval "
+    db.getSiblingDB('${MONGO_DATABASE}').${TRIAL_NETWORK_COLLECTION}.updateMany({}, {
+        \$rename: {
+            'github_6g_library_https_url': 'library_https_url',
+            'github_6g_library_commit_id': 'library_commit_id',
+            'github_6g_sandbox_sites_https_url': 'sites_https_url',
+            'github_6g_sandbox_sites_commit_id': 'sites_commit_id'
+        }
+    });
+    "
+
+    rm -r ${MONGO_EXPRESS_PATH}-*
+    git clone --depth 1 --branch ${MONGO_EXPRESS_VERSION} -c advice.detachedHead=false https://github.com/mongo-express/mongo-express.git ${MONGO_EXPRESS_PATH}
+    cd ${MONGO_EXPRESS_PATH}
+    yarn install
+    yarn build
+
+    cat <<EOF > /etc/systemd/system/mongo-express.service
+[Unit]
+Description=Mongo Express
+
+[Service]
+Type=simple
+WorkingDirectory=${MONGO_EXPRESS_PATH}
+ExecStart=/bin/bash -ac 'source ${BACKEND_PATH}/.env && yarn start'
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+    systemctl daemon-reload
+    systemctl restart mongo-express.service
+
+    rm -r ${POETRY_PATH}
+
+    echo "Upgrade to version ${TARGET_VERSION} completed"
