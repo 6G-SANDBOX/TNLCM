768f8bd9274cf8f9125986cb2323cdd06a093c70
CarlosAndreo <c.andreo@uma.es>
2025-03-05 20:25:10 +0000

    fix: typo

--- conf/__init__.py
+++ conf/__init__.py
@@ -3,5 +3,5 @@ from dotenv import load_dotenv
 from core.utils.os_handler import current_directory, join_path
 
 dotenv_path = join_path(current_directory(), ".env")
-# dotenv_path = join_path(current_directory()".env.dev")
+# dotenv_path = join_path(current_directory(), ".env.dev")
 load_dotenv(dotenv_path=dotenv_path)
