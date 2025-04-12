d200a9df16c9403ea133eaa32da419034b2e5741
CarlosAndreo <c.andreo@uma.es>
2025-04-12 11:02:30 +0000

    change: small

--- core/routes/sites.py
+++ core/routes/sites.py
@@ -103,7 +103,6 @@ class ComponentsAvailable(Resource):
             deployment_site_token = decode_base64(
                 encoded_data=request.headers["Deployment-Site-Token"]
             )
-
             current_user = get_current_user_from_jwt(jwt_identity=get_jwt_identity())
             if not current_user:
                 return {"message": "User not found"}, 404
